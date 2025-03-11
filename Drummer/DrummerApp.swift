4ab3789ffcf0d02c4e041f9fbfaa8ecdeba4c694
Mekhak <mekhakghapantsyan@simplytechnologies.com>
2025-03-11 09:42:11 +0400

    Initial Commit

---
+++ Drummer/DrummerApp.swift
@@ -0,0 +1,33 @@
+//
+//  DrummerApp.swift
+//  Drummer
+//
+//  Created by Mekhak Ghapantsyan on 3/11/25.
+//
+
+import SwiftUI
+
+@main
+struct DrummerApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
